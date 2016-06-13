7dd02332c21b2085425f5dbb0552477856e3b25c
codeangler <burnett.casey@gmail.com>
2016-06-13 16:10:07 -0600

    this commit includes ui-router;  home page load working with next step to load access to a panic-game.html partial

--- js/app.js
+++ js/app.js
@@ -1,1 +1,37 @@
-angular.module('extractionApp', []);
+angular.module('extractionApp', ['ui.router'])
+  .config(configRouter)
+  .controller('homeCtrl', homeController)
+  .controller('panicCtrl', panicController)
+
+  configRouter.$inject = ['$stateProvider', '$urlRouterProvider']
+
+  function configRouter($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home',{
+        url: '/',
+        templateUrl: 'partials/home.html',
+        controller: 'homeCtrl as hCtrl',
+      })
+      .state ('panicGame',{
+        url: '/panic-game',
+        templateUrl: 'partials/panic-game.html',
+        controller: 'panicCtrl as pCtrl'
+      })
+
+    $urlRouterProvider.otherwise('/')
+  }
+  // 
+
+// function that runs the "I-AWARE INTERVENTION GAME" Panic Control
+  function panicController() {
+    var pCtrl = this;
+    pCtrl.greeting = "panic controller greeting"
+    console.log("hello from " + pCtrl.greeting);
+  }
+
+//  homeCtrl as hCtrl
+  function homeController() {
+    var hCtrl = this;
+    hCtrl.greeting = 'home controller greeting'
+    console.log('hello from ' + hCtrl.greeting);
+  }
